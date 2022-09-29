aa1aaa44479cd7c56ff08be480cc81dea051836b
Asadsaleem123 <asad.saleem683@gmail.com>
2022-09-29 04:59:01 +0300

    third_commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-// let btn=document.getElementById("btn");
-// btn.addEventListener("click",function(){
-//   console.log("button is clicked")
-//  let generated=document.getElementById("generated");
-//  let gen=document.getElementById("gen");
-//  let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-// "/"];
-// let RandomNumber=Math.floor(Math.random)*characters.length;
-// console.log(RandomNumber);
-// })
-
 function App() {
    function Button(){
     let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
@@ -51,7 +40,7 @@ function App() {
             <div className='insecure'>
               <p>Never Use an insecure password again.</p>
             </div>
-            <input  type="text"id="set-length"/>
+            <input  type="text"id="set-length"placeholder="Enter Password Length"/>
              <button onClick={SetLength} id="length_btn">Set length</button>
             <button onClick={Button} id="btn" className='btn'>Generate passwords</button>
             

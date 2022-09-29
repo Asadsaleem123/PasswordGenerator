60ed2f1cb9b57d1e09b997b7b8f934a64a103869
Asadsaleem123 <asad.saleem683@gmail.com>
2022-09-29 04:29:38 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route exact path="/"element={<App/>}></Route>
+    </Routes>
+
+
+
+    <React.StrictMode>
+     
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
